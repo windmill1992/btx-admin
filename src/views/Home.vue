c5032d2e18421631362bfb241bceed67d625a837
yup <1182657869@qq.com>
2018-11-14 17:49:22 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -75,7 +75,7 @@
 export default {
   data() {
     return {
-      sysName: "抱团学后台管理系统",
+      sysName: "趣抢课后台管理系统",
       collapsed: false,
       sysUserName: "",
       sysUserAvatar: "",
--- src/views/finance/withdraw.vue
+++ src/views/finance/withdraw.vue
@@ -8,7 +8,6 @@
                     <el-option value="" label="全部"></el-option>
                     <el-option :value="1" label="进行中"></el-option>
                     <el-option :value="2" label="已完成"></el-option>
-                    <el-option :value="3" label="已失败"></el-option>
                 </el-select>
                 <el-button type="primary" class="fl" @click="search">搜索</el-button>
             </div>
@@ -16,14 +15,21 @@
         <el-col :span="24">
             <el-table :data="list" highlight-current-row v-loading="loading" border style="width: 100%;height: 90%;">
                 <el-table-column type="index" label="序号" width="80"></el-table-column>
-                <el-table-column prop="orderNo" label="订单编号" width="100"></el-table-column>
-                <el-table-column prop="merchantName" label="商家名称" min-width="220" show-overflow-tooltip></el-table-column>
-                <el-table-column prop="money" label="提现金额" width="120"></el-table-column>
-                <el-table-column prop="time" label="提现时间" width="150"></el-table-column>
+                <el-table-column prop="withdrawRecordId" label="订单编号" width="100"></el-table-column>
+                <el-table-column prop="userId" label="商家ID" width="100"></el-table-column>
+                <el-table-column prop="capital" label="提现金额" width="150"></el-table-column>
+                <el-table-column prop="applyTime" label="申请时间" width="250"></el-table-column>
+                <el-table-column label="状态" width="150">
+                    <template slot-scope="scope">
+                        <span v-if="scope.row.status == 1">申请中</span>
+                        <span v-else>已打款</span>
+                    </template>
+                </el-table-column>
                 <el-table-column label="操作" min-width="150">
                     <template slot-scope="scope">
                         <div class="flex fcen">
                             <el-button type="info" class="btn" @click="showDetail(scope.row)">详情</el-button>
+                            <el-button type="primary" class="btn" @click="sure(scope.row.withdrawRecordId)">已打款</el-button>
                         </div>
                     </template>
                 </el-table-column>
@@ -109,6 +115,9 @@ export default {
             this.detailInfo = Object.assign({}, row);
             this.showModal = true;
         },
+        sure(id) {
+            
+        }
     },
     mounted() {
         // this.getData();
